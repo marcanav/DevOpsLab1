6310b6662de4259e22043204fd1d3313c4da5691
Maria Nav <mnavar17@my.centennialcollege.ca>
2024-02-02 14:36:15 -0500

    Initial commit of DevOps Lab1

---
+++ DevOpsLab1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevOpsLab1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
